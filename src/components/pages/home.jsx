f0ce9685128af663f61cccedb31d33f5f92bf8a9
bahe <bahebdg@gmail.com>
2023-09-14 02:38:29 +0700

    header

--- src/components/pages/home.jsx
+++ src/components/pages/home.jsx
@@ -1,30 +1,39 @@
+import { Link } from "react-router-dom";
+
 export default function Home(){
     return(
-        <div className="font-roboto">
-            <div className="bg-primary-blue h-40">
-                <div className="text-primary-orange">MYHOTEL</div>
-                <div className="uppercase text-white">
-                    <span>home</span>
-                    <span>about</span>
-                    <span>service</span>
-                    <span>rooms</span>
-                    <span>pages</span>
-                    <span>contact</span>
-                </div>
-                <div className="">
-                    <div className="bg-white">                       
-                        <i class="ri-mail-line text-primary-orange">info@myhotel.yogya.com</i>
-                        <i class="ri-phone-fill text-primary-orange">+6274 5432 9876</i>
+        <div className="font-roboto bg-black h-screen">
+            <div className="bg-primary-blue h-24 flex justify-between">
+                <div className="text-4xl font-semibold text-primary-orange w-[25%] flex items-center justify-center">MYHOTEL</div>  
+                <div className="w-[75%] flex-row justify-center items-center">
+                    <div className="h-8 bg-white flex items-center justify-between px-6">
+                        <div className="flex justify-around text-xs">   
+                            <div className="flex items-center mr-2">
+                                <i class="ri-mail-line text-primary-orange"></i>     
+                                <span className="text-secondary-gray px-2">info@myhotel.yogya.com</span>
+                            </div>
+                            <div className="flex items-center mx-4">
+                                <i class="ri-phone-fill text-primary-orange"></i>
+                                <span className="text-secondary-gray px-2">+6274 5432 9876</span>
+                            </div>     
+                        </div>
+                        <div className="w-40 flex items-center justify-around">
+                            <i class="ri-facebook-line text-primary-orange"></i>
+                            <i class="ri-twitter-fill text-primary-orange"></i>
+                            <i class="ri-linkedin-line text-primary-orange"></i>
+                            <i class="ri-instagram-line text-primary-orange"></i>
+                            <i class="ri-youtube-line text-primary-orange"></i>
+                        </div>
                     </div>
-                    <div>
-                        <i class="ri-facebook-line text-primary-orange"></i>
-                        <i class="ri-twitter-fill text-primary-orange"></i>
-                        <i class="ri-linkedin-line text-primary-orange"></i>
-                        <i class="ri-instagram-line text-primary-orange"></i>
-                        <i class="ri-youtube-line text-primary-orange"></i>
+                    <div className="h-16 w-[500px] uppercase text-white text-sm flex items-center justify-start">
+                        <Link to='' className="mr-8 hover:text-primary-orange text-primary-orange">home</Link>
+                        <Link to='' className="mr-8 hover:text-primary-orange">about</Link>
+                        <Link to='' className="mr-8 hover:text-primary-orange">service</Link>
+                        <Link to='' className="mr-8 hover:text-primary-orange">rooms</Link>
+                        <Link to='' className="mr-8 hover:text-primary-orange">pages</Link>
+                        <Link to='' className="mr-8 hover:text-primary-orange">contact</Link>
                     </div>
-
-                </div>
+                </div>            
             </div>
         </div>
     )
