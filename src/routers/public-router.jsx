3dcab9d63ad973228adf22d274b88b91852d7331
bahe <bahebdg@gmail.com>
2023-09-15 23:46:35 +0700

    restore

---
+++ src/routers/public-router.jsx
@@ -0,0 +1,10 @@
+import { Navigate, Outlet } from "react-router-dom";
+
+export default function PublicRouter(){
+    return(
+        <div>
+            <Outlet />
+            <Navigate to='/home' />
+        </div>
+    )
+}
