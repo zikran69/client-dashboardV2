4a3642d7faa2f364e06a56c371da16dde2855a17
bahe-eman <bahebdg@gmail.com>
2023-09-15 16:22:31 +0700

    save point

--- src/component/service-card.jsx
+++ src/component/service-card.jsx
@@ -1,20 +1,22 @@
-export default function ServiceCard(){
+export default function ServiceCard({icon, title}){
     const hoverBoss = ()=>{
 
     }
     return(
-        <div className="">
-            <div onMouseEnter={hoverBoss} className="font-roboto w-[350px] h-[350px] border lg:drop-shadow-2xl drop-shadow-xl bg-white m-4 grid content-center">
-                <div className="border border-primary-gray w-fit h-fit p-1 rounded-sm">
-                    <div className="border border-primary-gray w-fit h-fit p-2 rounded-sm"><i class="fa fa-swimmer fa-2x text-primary-orange"></i></div> 
+        <div onMouseEnter={hoverBoss} className="font-roboto w-[350px] h-[350px] border lg:drop-shadow-2xl drop-shadow-xl bg-white m-4 px-8 grid gap-3 content-center text-center">
+            <div className="flex justify-center">
+                <div className="border border-solid border-zinc-200 w-fit h-fit p-1 rounded-sm">
+                    <div className="border border-so w-fit h-fit p-2 rounded-sm"><i class="fa fa-swimmer fa-2x text-primary-orange"></i></div> 
                 </div>
-                <div className="flex justify-between items-center capitalize font-semibold w-[140px]">
+            </div>
+            <div className="flex justify-center">
+                <div className="flex justify-between items-center capitalize font-semibold text-lg w-[160px]">
                     <span className="tracking-widest">sport</span>
                     <span className="tracking-widest">&</span>
                     <span className="tracking-widest">gaming</span>
                 </div>
-                <p className="font-raleway">Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem sed diam stet diam sed stet lorem.</p>
             </div>
+                <p className="font-raleway">Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem sed diam stet diam sed stet lorem.</p>
         </div>
     )
 }
