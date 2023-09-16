61b84a4af09ebfc92e73b05df0f836fbfef3cefa
zikran69 <zikzigi69@gmail.com>
2023-09-16 20:32:40 +0700

    menambahkan crousel belom jadi di home

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -1,16 +1,17 @@
 import { Routes, Route } from "react-router-dom";
-import PublicRouter from './public-router';
-import Home from "../pages/home"
+import PublicRouter from "./public-router";
+import Home from "../pages/home";
 import Card from "../component/card";
+import MyCarousel from "../component/carousel";
 
-
-export default function Router(){
-    return(
-        <Routes>
-            <Route path="/" element={<PublicRouter />} >
-                <Route path="/home" element={<Home />} />
-                <Route path="/card" element={<Card />} />
-            </Route>
-        </Routes>
-    )
+export default function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<PublicRouter />}>
+        <Route path="/MyCarousel" element={<MyCarousel />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/card" element={<Card />} />
+      </Route>
+    </Routes>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,30 @@
+import { MaterialTailwindTheme } from "@material-tailwind/react";
+import { transformWithEsbuild } from "vite";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       transitionProperty: {
-        'display': 'display',
+        display: "display",
       },
       colors: {
-        'primary-blue': '#0f172a',
-        'secondary-blue': '#78aed8',
-        'primary-gray': '#eff3f6',
-        'secondary-gray': '#b4b6b9',
-        'primary-orange': '#fa9b14',
-        'hover-blue': '#2563EB',
-        'hover-green': '#16A34A',
-        'hover-red': '#DC2626',
-        'hover-yellow': '#CA8A04'
+        "primary-blue": "#0f172a",
+        "secondary-blue": "#78aed8",
+        "primary-gray": "#eff3f6",
+        "secondary-gray": "#b4b6b9",
+        "primary-orange": "#fa9b14",
+        "hover-blue": "#2563EB",
+        "hover-green": "#16A34A",
+        "hover-red": "#DC2626",
+        "hover-yellow": "#CA8A04",
       },
       fontFamily: {
-        roboto: ['Roboto', 'sans-serif'],
-        raleway: ['Raleway', 'sans-serif'],
+        roboto: ["Roboto", "sans-serif"],
+        raleway: ["Raleway", "sans-serif"],
       },
     },
   },
   plugins: [],
-}
-
+};
