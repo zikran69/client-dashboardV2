587a3a39e91e59c533d74b16cecfa60eab4ff9d5
zikran69 <zikzigi69@gmail.com>
2023-10-05 04:34:43 +0700

    lanjut ngerjain user page

--- src/pages/Userpage.jsx
+++ src/pages/Userpage.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Header from "../file-bahe/header";
+import Footer from "../component/footer";
 
 function Userpage() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      {/* main content */}
+      <div className="p-2 m-2">
+        <h1 className="font-bold">Welcome:</h1>
+        <img
+          className="w-[150px] h-[150px] rounded-full my-4 shadow-xl outline outline-offset-2"
+          src="https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg"
+        />
+      </div>
+
+      <Footer />
+    </>
+  );
 }
 
 export default Userpage;
