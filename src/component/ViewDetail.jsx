97e741c2c34f86f6240c14c59cd91a54d56eedf1
bahe <123801100+bahe-eman@users.noreply.github.com>
2023-12-07 19:47:22 +0700

    Update ViewDetail.jsx

--- src/component/ViewDetail.jsx
+++ src/component/ViewDetail.jsx
@@ -29,6 +29,8 @@ export default function ViewDetail() {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
     prevArrow: <button className="slick-prev"></button>,
     nextArrow: <button className="slick-next"></button>,
   };
